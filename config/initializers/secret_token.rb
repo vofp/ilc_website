12b8d412761e7a0dcc459e2e3079a3b99b4d0238
Francis Vo <firefreakfrancis@gmail.com>
2011-02-17 22:14:07 -0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IlcWebsite::Application.config.secret_token = 'b5296b097e754997a212265532904bdc7f12a621937d003226e011d7d794930766963bb21f391bdcabd27caa9693a8e78120d7e11c89fecd242509d9c9fbbf60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+IlcWebsite::Application.config.session_store :cookie_store, :key => '_ilc_website_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# IlcWebsite::Application.config.session_store :active_record_store
